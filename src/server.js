5f59b693facb55e033c6e26ccf77a1f5022e9a46
Oleh <babijchik@gmail.com>
2026-08-08 19:43:48 +0300

    Fix get notes

--- src/server.js
+++ src/server.js
@@ -32,9 +32,11 @@ app.get('/notes', (req, res) => {
   });
 });
 
-app.get('/notes/:noteId', (reg, res) => {
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
   res.status(200).json({
-    message: 'Retrieved note with ID: id_param',
+    message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
