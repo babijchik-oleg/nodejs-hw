0ca45523e9f7abc631bd6a3099d2f50df9fcb9dc
Oleh <babijchik@gmail.com>
2026-09-13 19:13:00 +0300

    fix import

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { updateUserAvatar } from '../controllers/userController.js';
-import { authenticate } from '../middlewares/authenticate.js';
-import { upload } from '../middlewares/multer.js';
+import { authenticate } from '../middleware/authenticate.js';
+import { upload } from '../middleware/multer.js';
 
 const router = Router();
 
